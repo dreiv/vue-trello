3c890451d284cc761718f3085e7877729c7c9281
Andrei Voicu <dreivoicu@gmail.com>
2020-08-29 18:59:35 +0300

    add description

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -2,6 +2,7 @@
   <div class="backdrop" @click.self="close">
     <div class="dialog">
       {{ task.name }}
+      <textarea class="description" :value="task.description" />
     </div>
   </div>
 </template>
@@ -44,9 +45,17 @@ export default {
 .dialog {
   background-color: white;
   width: 100%;
-  max-width: 60rem;
+  max-width: 40rem;
   margin: 10% auto;
   padding: 1rem;
   border-radius: 0.25rem;
+}
+
+.description {
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+  margin-top: 0.5rem;
+  padding: 0.5rem;
 }
 </style>
